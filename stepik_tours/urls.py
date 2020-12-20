150749b61c1a8368b7eab30b1ba409685dc87b43
Vladimir_Polyakov <vlpolyakov@mail.ru>
2020-12-20 21:11:05 +0400

    Исправил ошибки в проектном задании главы 1.

--- stepik_tours/urls.py
+++ stepik_tours/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from tours.views import MainView, DepartureView, TourView
+
+from tours.views import custom_handler404, custom_handler500, DepartureView, MainView, TourView
+
+handler404 = custom_handler404
+handler500 = custom_handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- tours/views.py
+++ tours/views.py
@@ -1,13 +1,23 @@
+from django.http import HttpResponseNotFound, HttpResponseServerError
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
-from django.views import View
+
+
+def custom_handler404(request, exception):
+    return HttpResponseNotFound('<h1>404<h1><h2>The requested resource was not found on the server.</h2>')
+
+
+def custom_handler500(request):
+    return HttpResponseServerError('<h1>500</h1><h2>That happened internal server error.</h2>')
 
 
 class MainView(View):
 
     def get(self, request):
-        return render(request, 'tours/index.html')
+        return render(request, 'index.html')
 
 
 class DepartureView(View):
